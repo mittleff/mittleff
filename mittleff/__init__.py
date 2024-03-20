9ac601be62ab9c1dbd80ddc7f1791d55bba76932
Victor Santos (vct) <victor_santos@fisica.ufc.br>
2024-03-20 20:05:14 -0300

    Adiciona estrutura básica do algoritmo
    De acordo com a listagem do código da Frontiers
    
    - Adiciona python-flint como dependência do pacote

--- mittleff/__init__.py
+++ mittleff/__init__.py
@@ -1,2 +1,28 @@
+import flint
+
+def Taylor(z, α, β, ε): return 0.0
+def Recursion(z, α, β, ε): return 0.0
+def Asymptotics(z, α, β, ε): return 0.0
+def IntegralRep(z, α, β, ε): return 0.0
+
+π = flint.arb.pi()
+
 def MittLeff(z, α, β, ε):
-    return 1.0
+    r0 = 0.9 # Taylor series upper limit
+    δ = π * α / 10.0 # Stokes line region
+    r1 = 0.5
+    if abs(z) < r1:
+        # Evaluate the Taylor Series
+        return Taylor(z, α, β, ε)
+    else:
+        if α > 1:
+            # Apply the Recursion Relation
+            return Recursion(z, α, β, ε)
+        else:
+            r2 = 2.0
+            if abs(z) > r2:
+                # Evaluate the Asymptotic Series
+                return Asymptotics(z, α, β, ε)
+            else:
+                #  Evaluate the Integral Representation
+                return IntegralRep(z, α, β, ε)
