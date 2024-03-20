71647ed273d968bf1b61ed02fa1487e3939c142e
Victor Santos (vct) <victor_santos@fisica.ufc.br>
2024-03-20 20:43:01 -0300

    Corrige condição para entrar no algoritmo recursiv
    alpha >= 1 (olhar code listing)

--- mittleff/__init__.py
+++ mittleff/__init__.py
@@ -4,29 +4,33 @@ from flint import arb, acb
 def MittLeff(z: complex, α: float, β: float, ε: float = 1e-15):
     return _ML(acb(z.real, z.imag), arb(α), arb(β), arb(ε))
 
-def _Taylor(z, α, β, ε): return 0.0
-def _Recursion(z, α, β, ε): return 0.0
-def _Asymptotics(z, α, β, ε): return 0.0
-def _IntegralRep(z, α, β, ε): return 0.0
-
-π = flint.arb.pi()
-
 def _ML(z: acb, α: arb, β: arb, ε: arb) -> acb:
-    r0 = 0.9 # Taylor series upper limit
-    δ = π * α / 10.0 # Stokes line region
-    r1 = 0.5
-    if abs(z) < r1:
+    π = flint.arb.pi()
+    r1 = arb(0.9) # Taylor series upper limit
+    δ = π * α / 10.0 # Stokes line region        
+    if z.abs_lower() < r1:
         # Evaluate the Taylor Series
         return _Taylor(z, α, β, ε)
     else:
-        if α > 1:
+        if α >= 1:
             # Apply the Recursion Relation
             return _Recursion(z, α, β, ε)
         else:
-            r2 = 2.0
-            if abs(z) > r2:
+            r2 = _compute_r2(α, ε)
+            if z.abs_lower() > r2:               
                 # Evaluate the Asymptotic Series
                 return _Asymptotics(z, α, β, ε)
             else:
                 #  Evaluate the Integral Representation
                 return _IntegralRep(z, α, β, ε)
+
+def _Taylor(z: acb, α: arb, β: arb, ε: arb) -> acb: return acb(0)
+def _Recursion(z: acb, α: arb, β: arb, ε: arb) -> acb: return acb(0)
+def _Asymptotics(z: acb, α: arb, β: arb, ε: arb) -> acb: return acb(0)
+def _IntegralRep(z: acb, α: arb, β: arb, ε: arb) -> acb: return acb(0)
+
+def _compute_r2(α: arb,  ε: arb) -> arb:
+    π = flint.arb.pi()
+    C0 = arb("1.3")**(arb("1") - α)/(π * arb.sin(π * α))
+    r2 = (arb("-2") * arb.log(ε/C0))**α
+    return r2
