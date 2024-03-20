8a9574156963e7a8ac07fc901ff86ec4a89eb1f2
Victor Santos (vct) <victor_santos@fisica.ufc.br>
2024-03-20 20:14:35 -0300

    Adiciona função de interface
    A função que o usuário irá utilizar é =MittLeff=, enquanto as funções
    que iniciam com underscore são internas.

--- mittleff/__init__.py
+++ mittleff/__init__.py
@@ -1,28 +1,32 @@
 import flint
+from flint import arb, acb
 
-def Taylor(z, α, β, ε): return 0.0
-def Recursion(z, α, β, ε): return 0.0
-def Asymptotics(z, α, β, ε): return 0.0
-def IntegralRep(z, α, β, ε): return 0.0
+def MittLeff(z: complex, α: float, β: float, ε: float = 1e-15):
+    return _ML(acb(z.real, z.imag), arb(α), arb(β), arb(ε))
+
+def _Taylor(z, α, β, ε): return 0.0
+def _Recursion(z, α, β, ε): return 0.0
+def _Asymptotics(z, α, β, ε): return 0.0
+def _IntegralRep(z, α, β, ε): return 0.0
 
 π = flint.arb.pi()
 
-def MittLeff(z, α, β, ε):
+def _ML(z: acb, α: arb, β: arb, ε: arb) -> acb:
     r0 = 0.9 # Taylor series upper limit
     δ = π * α / 10.0 # Stokes line region
     r1 = 0.5
     if abs(z) < r1:
         # Evaluate the Taylor Series
-        return Taylor(z, α, β, ε)
+        return _Taylor(z, α, β, ε)
     else:
         if α > 1:
             # Apply the Recursion Relation
-            return Recursion(z, α, β, ε)
+            return _Recursion(z, α, β, ε)
         else:
             r2 = 2.0
             if abs(z) > r2:
                 # Evaluate the Asymptotic Series
-                return Asymptotics(z, α, β, ε)
+                return _Asymptotics(z, α, β, ε)
             else:
                 #  Evaluate the Integral Representation
-                return IntegralRep(z, α, β, ε)
+                return _IntegralRep(z, α, β, ε)
