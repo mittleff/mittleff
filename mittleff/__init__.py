675bc3608e03bb301f5cf0a7c417c6ff82524111
Victor Santos (vct) <victor_santos@fisica.ufc.br>
2024-03-23 18:30:10 -0300

    Modifica armazenamento em logs

--- mittleff/__init__.py
+++ mittleff/__init__.py
@@ -1,31 +1,43 @@
+import logging
 import flint
 from flint import arb, acb
 from .partition import in_region_G0, in_region_G1, in_region_G2, in_region_G3, in_region_G4, in_region_G5, in_region_G6
 from .algorithm import mittleff0, mittleff1, mittleff2, mittleff3, mittleff4, mittleff5, mittleff6
 
+logger = logging.getLogger(__name__)
+
 def mittleff(α: arb, β: arb, z: acb, ε: arb) -> acb:
-    print(α, β, z, ε)
+    logger.info(f'''
+######################################################################
+# Parameters:
+#   α = {α}
+#   β = {β}
+#   z = {z}
+#   ε = {ε}
+######################################################################
+''')
     if in_region_G0(z, α, ε):
         ################################
         # Evaluate using Taylor series #
         ################################
-        print(f"region 0")
+        logger.info(f"\n{z} ∈ G_0")
         return mittleff0(α, β, z, ε)
     else:
         if α > 1:
+            logger.debug(f"α={α} > 1: applying recursive relation")
             ###############################################
             # Apply recursive relation (2.14) from thesis #
             ###############################################
             π = flint.arb.pi()
             one, two, J = arb("1"), arb("2"), acb(0+1j)
             m = int(float(arb.mid(arb.ceil(α))))
-            print('m = ', m)
+            #print('m = ', m)
             s = acb(0+0j)
             a = α / m
             #print(s)
             for h in range(0, m):
                 zp = z.pow(one / m) * acb.exp(two * π * J * h / m)
-                print(f"h = {h}", _mittleff(a, β, zp, ε))
+                #print(f"h = {h}", mittleff(a, β, zp, ε))
                 s += mittleff(a, β, zp, ε)
             s = (one/m) * s
             #print('s = ', s)
@@ -48,22 +60,22 @@ def mittleff(α: arb, β: arb, z: acb, ε: arb) -> acb:
             # return one/(two*m + one) * s
         else:
             if in_region_G1(z, α, ε):
-                #print(f"region 1")
+                logger.info(f"\n{z} ∈ G_1")
                 return mittleff1(α, β, z, ε)
             elif in_region_G2(z, α, ε):
-                #print(f"region 2")
+                logger.info(f"\n{z} ∈ G_2")
                 return mittleff2(α, β, z, ε)
             elif in_region_G3(z, α, ε):
-                #print(f"region 3")
+                logger.info(f"\n{z} ∈ G_3")
                 return mittleff3(α, β, z, ε)
             elif in_region_G4(z, α, ε):
-                #print(f"region 4")
+                logger.info(f"\n{z} ∈ G_4")
                 return mittleff4(α, β, z, ε)
             elif in_region_G5(z, α, ε):
-                print(f"region 5")
+                logger.info(f"\n{z} ∈ G_5")
                 return mittleff5(α, β, z, ε)
             elif in_region_G6(z, α, ε):
-                print(f"region 6")
+                logger.info(f"\n{z} ∈ G_6")
                 return mittleff6(α, β, z, ε)
             else:
-                print("kasjdhaksjdhaaaa")
+                raise Exception(f"Could not find region for {z}") 
--- mittleff/__main__.py
+++ mittleff/__main__.py
@@ -1,4 +1,5 @@
-from mittleff import MittLeff
+from flint import arb, acb
+from mittleff import mittleff
 
-print(MittLeff(1, 1, 0.6, 1.0e-15)) # => 1.822118800390509
-print(MittLeff(0.5, 1.0, +1.00809273e+01+2.22251668e+00j, 1.0e-15))
+print(mittleff(arb(1), arb(1), acb(0.6), arb(1e-15))) # => 1.822118800390509
+#print(mittleff(0.5, 1.0, +1.00809273e+01+2.22251668e+00j, 1.0e-15))
